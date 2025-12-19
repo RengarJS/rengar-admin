d3122c04482a76e930dc573869457d709b1b01a4
ashuicoder <1249735120@qq.com>
2025-12-19 09:11:45 +0800

    代码优化

---
+++ src/layouts/components/common/SettingControl.vue
@@ -0,0 +1,17 @@
+<template>
+  <NTooltip placement="bottom">
+    <template #trigger>
+      <div class="cursor-pointe flex-center rounded-sm p-1 hover:text-primary" @click="appStore.toggleConfigDrawer">
+        <SvgIcon icon="clarity:settings-line"></SvgIcon>
+      </div>
+    </template>
+    <span>系统配置</span>
+  </NTooltip>
+</template>
+
+<script setup lang="ts">
+import { useAppStore } from '@/stores'
+const appStore = useAppStore()
+</script>
+
+<style scoped></style>
---
+++ src/layouts/components/common/ThemeControl.vue
@@ -0,0 +1,19 @@
+<template>
+  <NTooltip placement="bottom">
+    <template #trigger>
+      <div class="flex-center cursor-pointer rounded-sm p-1 hover:text-primary" @click="appStore.toggleTheme">
+        <SvgIcon v-if="appStore.themeMode === 'light'" icon="line-md:sunny"></SvgIcon>
+        <SvgIcon v-else-if="appStore.themeMode === 'dark'" icon="line-md:moon-filled"></SvgIcon>
+        <SvgIcon v-else icon="material-symbols:hdr-auto"></SvgIcon>
+      </div>
+    </template>
+    <span>主题</span>
+  </NTooltip>
+</template>
+
+<script setup lang="ts">
+import { useAppStore } from '@/stores'
+const appStore = useAppStore()
+</script>
+
+<style scoped></style>
